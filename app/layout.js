d638e62cb2370f0381db33e550d21bd263600610
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-06-17 15:15:10 +0200

    updated to chinzel font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Cinzel } from "next/font/google";
 import "./globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+const chinzel = Cinzel({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body className={chinzel.className}>{children}</body>
 		</html>
 	);
 }
