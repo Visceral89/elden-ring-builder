3ccb7c0dff95c6efbd9617bb56d92dbe7c716342
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-06-27 16:24:55 +0200

    redid weapons

--- app/utils/getWeapons.js
+++ app/utils/getWeapons.js
@@ -2,10 +2,31 @@ import weaponData from "@/app/data/weapons.json";
 
 export const allWeapons = weaponData;
 
-export const getWeaponByCategory = (category) => {
-	return weaponData.filter((weapon) => weapon.category === category);
+export const getWeaponsByCategory = (category) => {
+	return weapondata.filter((w) => w.category === category);
 };
 
 export const getWeaponAttack = (name) => {
-    return const 
-}
+	const weapon = weaponsData.find((w) => w.name === name);
+	return weapon ? weapon.attack : null;
+};
+
+export const getWeaponScalesWith = (name) => {
+	const weapon = weaponsData.find((w) => w.name === name);
+	return weapon ? weapon.scalesWith : null;
+};
+
+export const getWeaponRequiredAttributes = (name) => {
+	const weapon = weaponsData.find((w) => w.name === name);
+	return weapon ? weapon.requiredAttributes : null;
+};
+
+export const getWeaponWeight = (name) => {
+	const weapon = weaponsData.find((w) => w.name === name);
+	return weapon ? weapon.weight : null;
+};
+
+export const getWeaponDesc = (name) => {
+	const weapon = weaponsData.find((w) => w.name === name);
+	return weapon ? weapon.description : null;
+};
