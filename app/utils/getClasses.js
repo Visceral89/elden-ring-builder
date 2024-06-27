efed152d10b72d50ba892957d96c0072d3ede5f7
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-06-27 10:50:33 +0200

    Create getClasses.js

---
+++ app/utils/getClasses.js
@@ -0,0 +1,8 @@
+import classesData from "@/app/data/classes.json";
+export const classNames = classesData.name;
+
+export const getClassStats = (name) => {
+	return ();
+};
+
+export { classNames };
